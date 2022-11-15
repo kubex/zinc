e69336273da717627078f46f562eb680e17019ee
Brooke Bryan <brooke@bajb.net>
2022-11-15 21:17:27 +0000

    Panels should not push a colspan

--- Layout/Components/Panel/ts/Component.ts
+++ Layout/Components/Panel/ts/Component.ts
@@ -1,5 +1,5 @@
 import {html, LitElement, unsafeCSS} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement} from 'lit/decorators.js';
 // @ts-ignore
 import styles from '../scss/Styles.scss';
 
@@ -7,16 +7,6 @@ import styles from '../scss/Styles.scss';
 export class ZincPanel extends LitElement {
     static get styles() {
         return [unsafeCSS(styles)];
-    }
-
-
-    @property()
-    colspan = "3"//of 12
-
-
-    connectedCallback() {
-        super.connectedCallback();
-        this.classList.add("col-span-" + this.colspan);
     }
 
     render() {

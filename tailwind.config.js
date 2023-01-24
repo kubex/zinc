7eb33e062b876337fa6c2b3d0e38a149932f65e7
Brooke Bryan <brooke@bajb.net>
2023-01-24 19:55:56 +0000

    Updated dark theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content:     [
+  content: [
     "./scss/*.scss",
     "./ts/*.ts",
     "./demo/*.html",
@@ -14,7 +14,19 @@ module.exports = {
   darkMode:    'class',
   theme:       {
     colors:     {
-      darkBorder: colors.slate["700"],
+      dark:       {
+        '50':  '#f8fafc',
+        '100': '#f1f5f9',
+        '200': '#e2e8f0',
+        '300': '#cbd5e1',
+        '400': '#94a3b8',
+        '500': '#64748b',
+        '600': '#475569',
+        '700': '#3B444B',
+        '800': '#1D2129',
+        '900': '#191D23'
+      },
+      darkBorder: "#343840",
       darkTxt:    {
         900: 'rgba(255,255,255, 1)',
         800: 'rgba(255,255,255, 0.87)',
@@ -52,18 +64,6 @@ module.exports = {
         400: 'rgba(0, 0, 0, 0.40)',
       },
 
-      dark: {
-        '50':  '#f8fafc',
-        '100': '#f1f5f9',
-        '200': '#e2e8f0',
-        '300': '#cbd5e1',
-        '400': '#94a3b8',
-        '500': '#64748b',
-        '600': '#475569',
-        '700': '#334155',
-        '800': '#18202f',
-        '900': '#0f172a'
-      },
       transparent: 'transparent',
 
       'white': '#FFFFFF',

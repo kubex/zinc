a427b79f97aebe18fa74cbe2b3c6810c0334c8b8
Brooke Bryan <brooke@bajb.net>
2023-06-19 15:42:56 +0100

    Switch to caption to avoid html conflict

--- Elements/Form/ts/Group.ts
+++ Elements/Form/ts/Group.ts
@@ -10,15 +10,15 @@ export class ZincFormGroup extends ZincElement {
         return [unsafeCSS(styles)];
     }
 
-    @property({attribute: 'title', type: String, reflect: true})
-    private title;
+    @property({attribute: 'caption', type: String, reflect: true})
+    private caption;
     @property({attribute: 'description', type: String, reflect: true})
     private description;
 
 
     render() {
         return html`
-          <div><h2 class="text-base font-semibold leading-7">${this.title}</h2>
+          <div><h2 class="text-base font-semibold leading-7">${this.caption}</h2>
             <p class="mt-1 text-sm leading-6">${this.description}</p></div>
           <div class="fg-inputs">
             <slot></slot>

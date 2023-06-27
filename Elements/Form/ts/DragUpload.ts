9089e3e721301c4a549b3ce04abdc882bb73868f
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-27 18:14:12 +0100

    Style drag upload box

--- Elements/Form/ts/DragUpload.ts
+++ Elements/Form/ts/DragUpload.ts
@@ -1,11 +1,20 @@
 import {html, unsafeCSS} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from "lit/decorators.js";
 import {ZincElement} from "../../../ts/element";
 
 import styles from '../scss/DragUpload.scss';
 
 @customElement('zn-drag-upload')
 export class ZincDragUpload extends ZincElement {
+
+  @property({attribute: 'text', type: String, reflect: true})
+  private text;
+  @property({attribute: 'types', type: String, reflect: true})
+  private types;
+  @property({attribute: 'size', type: String, reflect: true})
+  private size;
+
+
   static get styles() {
     return [unsafeCSS(styles)];
   }
@@ -14,12 +23,24 @@ export class ZincDragUpload extends ZincElement {
     return this;
   }
 
+  getHumanTypes() {
+    return this.types.split(',').map((type) => {
+      return type.replace('image/', '').toUpperCase();
+    }).join(', ');
+  }
+
   render() {
     return html`
-      <div>
-        <span class="bg-warning px-lg py-lg">
-          <slot></slot>
-        </span>
+      <div id="drag-upload"
+           class="flex flex-col items-center justify-center bg-white p-md rounded-md border-dashed border-dark-200 border-2">
+        <p>
+          ${this.text}
+        </p>
+        <p>
+            (${this.getHumanTypes()}) up to (${this.size}MB)
+        </p>
+        <input type="file" id="fileUpload" accept="${this.types}" size="${this.size}" class="!hidden"/>
+        <label for="fileUpload">Upload</label>
       </div>`
   }
 }

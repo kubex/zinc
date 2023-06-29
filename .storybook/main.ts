cd5f1a1736301ba5d345f70fe17a42a3acaf1d6b
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-29 22:55:20 +0100

    Add columns to story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type {StorybookConfig} from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
-  stories: ["../elements/**/*.mdx", "../elements/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../elements/**/*.mdx", "../elements/**/*.stories.@(js|jsx|ts|tsx)", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

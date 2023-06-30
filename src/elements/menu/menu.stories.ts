4d805142f81f3897257d78386f4bf0abd696dde7
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-30 11:05:09 +0100

    fix menu demo add css to head

--- src/elements/menu/menu.stories.ts
+++ src/elements/menu/menu.stories.ts
@@ -6,32 +6,9 @@ export default {
 
 export const Default = () =>
   html`
-    <zn-panel stat single>Panel Content</zn-panel>
-    <zn-panel>Panel Content</zn-panel>
-    <zn-panel caption="Subscriptions">Panel Content</zn-panel>
-    <zn-panel rows=5 caption="Billing"
-              navigation='[{"title": "Transactions", "path": "/", "default":true},
-            {"title": "Payments", "path": "/communication"},
-            {"title": "Refunds", "path": "/communication"}]'>Panel
-      Content
-    </zn-panel>
-    <zn-panel caption="Payment Methods" rows="2">
-      <zn-icon slot="actions" src="settings" size="24" library="material"></zn-icon>
-      <zn-icon slot="actions" src="add" size="24" library="material"></zn-icon>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-      Panel Content<br/>
-    </zn-panel>
-
-    <zn-panel caption="Payment Methods">
-      <zn-icon slot="actions" src="settings" size="24" library="material"></zn-icon>
-      Panel Content
-      <span slot="footer">Footer Content</span>
-    </zn-panel>
+    <zn-tile>
+      <zn-menu actions='[{"title": "Update", "path": "/chargehive/invoices", "target":"slide-content"},
+            {"title": "Suspend", "path": "/communication"}]'></zn-menu>
+    </zn-tile>
+    <hr>
   `;

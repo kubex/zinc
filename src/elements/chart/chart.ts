e4cff51246c7d39b5d0fe5790715e781fa0440e6
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-30 11:24:41 +0100

    Add multi line and bar charts Add ability to pass through data

--- src/elements/chart/chart.ts
+++ src/elements/chart/chart.ts
@@ -1,5 +1,5 @@
 import {html, unsafeCSS} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import {ZincElement} from "../../ts/element";
 import {Chart, registerables} from 'chart.js'
 import {htmlLegendPlugin} from "./HtmlLegendPlugin";
@@ -10,6 +10,10 @@ import styles from './chart.scss';
 export class ZincChip extends ZincElement {
   static styles = unsafeCSS(styles);
   private myChart: any;
+
+  @property({attribute: 'datasets', type: Array, reflect: true}) public datasets = [];
+  @property({attribute: 'labels', type: Array, reflect: true}) public labels = [];
+  @property({attribute: 'type', type: String, reflect: true}) public type = 'line';
 
   constructor() {
     super();
@@ -26,23 +30,13 @@ export class ZincChip extends ZincElement {
   firstUpdated() {
     const ctx = (this.renderRoot.querySelector('#myChart2') as HTMLCanvasElement).getContext('2d');
 
+    const drawBackground = this.datasets.length <= 1;
+
     const config = {
-      type: 'line',
+      type: this.type,
       data: {
-        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        datasets: [
-          {
-            data: [
-              {x: "Mon", y: 20000},
-              {x: "Tue", y: 20400},
-              {x: "Wed", y: 19000},
-              {x: "Thu", y: 18700},
-              {x: "Fri", y: 18942},
-              {x: "Sat", y: 19243},
-              {x: "Sun", y: 19400},
-            ],
-          }
-        ]
+        labels: this.labels,
+        datasets: this.datasets
       },
       options: {
         layout: {
@@ -57,8 +51,8 @@ export class ZincChip extends ZincElement {
           line: {
             borderWidth: 3,
             borderColor: 'rgb(60, 216, 187)',
-            backgroundColor: 'rgba(60, 216, 187, 0.1)',
-            fill: true,
+            backgroundColor: drawBackground ? 'rgba(60, 216, 187, 0.1)' : 'rgba(0, 0, 0, 0)',
+            fill: drawBackground,
           }
         },
         scales: {

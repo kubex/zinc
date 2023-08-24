dd55bc4f9e0922191c1ef51f08c09e4ac99a8ecb
Tom Kay <oridan82@gmail.com>
2023-08-24 16:40:52 +0100

    add form submission for text input

--- src/FormElements/TextInput/textInput.ts
+++ src/FormElements/TextInput/textInput.ts
@@ -4,24 +4,47 @@ import {customElement, property} from 'lit/decorators.js';
 import styles from './index.scss';
 
 @customElement('zn-text-input')
-export class TextInput extends LitElement {
-  @property({attribute: 'id', type: String, reflect: true}) id;
-  @property({attribute: 'name', type: String, reflect: true}) name;
-  @property({attribute: 'prefix', type: String, reflect: true}) prefix;
-  @property({attribute: 'placeholder', type: String, reflect: true}) placeholder;
-  @property({attribute: 'value', type: String, reflect: true}) value;
-  @property({attribute: 'type', type: String, reflect: true}) type = 'text';
+export class TextInput extends LitElement
+{
+  @property({type: String, reflect: true}) id: string;
+  @property({type: String, reflect: true}) name: string;
+  @property({type: String, reflect: true}) prefix: string;
+  @property({type: String, reflect: true}) placeholder: string;
+  @property({type: String, reflect: true}) value: string;
+  @property({type: String, reflect: true}) type: string = 'text';
 
   static styles = unsafeCSS(styles);
 
-  render() {
+  private internals: ElementInternals;
+
+  static get formAssociated()
+  {
+    return true;
+  }
+
+  constructor()
+  {
+    super();
+    this.internals = this.attachInternals();
+    this.internals.setFormValue(this.value);
+  }
+
+  render()
+  {
     return html`
       ${this.prefix ? html`<span>${this.prefix}</span>` : ''}
       <input id="${this.id}"
              name="${this.name}"
              type="text"
              placeholder="${this.placeholder}"
-             value="${this.value}"/>
-      <slot></slot>`
+             value="${this.value}"
+             @change="${this._handleChange}"/>
+      <slot></slot>`;
+  }
+
+  _handleChange(e)
+  {
+    this.value = e.target.value;
+    this.internals.setFormValue(this.value);
   }
 }

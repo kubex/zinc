08c88aee863828188307a386d459c915f8f4b0dc
Tom Kay <oridan82@gmail.com>
2023-08-29 16:21:02 +0100

    update confirm modal

--- src/ConfirmModal/confirmModal.ts
+++ src/ConfirmModal/confirmModal.ts
@@ -1,95 +1,88 @@
 import {html, LitElement, unsafeCSS} from "lit";
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, query} from 'lit/decorators.js';
 
 import styles from './index.scss';
 import {PropertyValues} from "@lit/reactive-element";
 
 @customElement('zn-confirm-modal')
-export class ConfirmModal extends LitElement {
+export class ConfirmModal extends LitElement
+{
+  @query('dialog', true)
+  private _dialog: HTMLDialogElement;
+
   static styles = unsafeCSS(styles);
 
   @property({type: String, reflect: true}) trigger: string = '';
   @property({type: String, reflect: true}) title: string = '';
   @property({type: String, reflect: true}) content: string = '';
   @property({type: String, reflect: true}) action: string = '';
-  @property({type: String, reflect: true}) method: string = 'post';
+  @property({type: String, reflect: true}) method: string = '';
   @property({type: String, reflect: true}) type: string = 'warning';
   @property({type: String, reflect: true}) confirmText: string = "Confirm";
   @property({type: String, reflect: true}) cancelText: string = "Cancel";
 
 
-  getIcon() {
+  getIcon()
+  {
     const src = {
       'warning': 'priority_high',
       'error': 'priority_high',
       'success': 'check',
       'info': 'help'
-    }
+    };
 
 
     return html`
       <zn-icon slot="primary" src=${src[this.type]} size="24" library="mio"></zn-icon>
-    `
+    `;
   }
 
-  render() {
+  render()
+  {
     const icon = this.getIcon();
     return html`
       <dialog class="type-${this.type}">
         <!-- default dialog close button -->
         ${icon}
         <h2 class="title">${this.title}</h2>
-        <form action="${this.action}" method="${this.method}">
+        <form .action="${this.action}" .method="${this.method}">
           <p>${this.content}</p>
           <div class="button-group">
-            <button class="button--secondary" @click="${this.closeDialog}">${this.cancelText}</button>
+            <button type="button" class="button--secondary" @click="${this.closeDialog}">${this.cancelText}</button>
             <button type="submit">${this.confirmText}</button>
           </div>
         </form>
       </dialog>`;
   }
 
-  connectedCallback() {
+  connectedCallback()
+  {
     super.connectedCallback();
-    const trigger = document.querySelector('[modal-trigger="' + this.trigger + '"]');
-    if (trigger) {
-      trigger.addEventListener('click', this.toggleDialog.bind(this));
+    const trigger = document.querySelector('#' + this.trigger);
+    if (trigger)
+    {
+      trigger.addEventListener('click', this.openDialog.bind(this));
     }
   }
 
-  disconnectedCallback() {
+  disconnectedCallback()
+  {
     super.disconnectedCallback();
-    const trigger = document.querySelector('[modal-trigger="' + this.trigger + '"]');
-    if (trigger) {
-      trigger.removeEventListener('click', this.toggleDialog.bind(this));
+    const trigger = document.querySelector('#' + this.trigger);
+    if (trigger)
+    {
+      trigger.removeEventListener('click', this.openDialog.bind(this));
     }
   }
 
-  protected firstUpdated(_changedProperties: PropertyValues) {
-    super.firstUpdated(_changedProperties);
-    // If we need to hook into the dialog events, we can do it here
-    const dialog = this.shadowRoot.querySelector('dialog');
-    dialog.addEventListener('close', this.closeDialog.bind(this));
-    dialog.addEventListener('open', this.openDialog.bind(this));
+  closeDialog()
+  {
+    this._dialog.close();
   }
 
-  toggleDialog() {
-    const dialog = this.shadowRoot.querySelector('dialog');
-    if (dialog.hasAttribute('open')) {
-      this.closeDialog()
-    } else {
-      this.openDialog()
-    }
-  }
-
-  closeDialog() {
-    const dialog = this.shadowRoot.querySelector('dialog');
-    dialog.close();
-  }
-
-  openDialog() {
-    const dialog = this.shadowRoot.querySelector('dialog');
-    dialog.showModal();
+  openDialog()
+  {
+    this._dialog.showModal();
   }
 }
 

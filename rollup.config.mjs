53af904352ff5549aeaa4e07e7a74930804bcd10
Tom Kay <oridan82@gmail.com>
2023-09-13 13:47:09 +0100

    update typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
 // postcss

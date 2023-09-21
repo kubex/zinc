158874ed3c91e75b521996eeba31784903d9bce3
Kyle Essex <kylegavinessex@hotmail.com>
2023-09-21 15:11:41 +0100

    update columns to have a better layout structure

--- src/Columns/columns.ts
+++ src/Columns/columns.ts
@@ -3,101 +3,53 @@ import {customElement, property} from 'lit/decorators.js';
 
 import styles from './index.scss';
 
-@customElement('zn-cols')
+@customElement('zn-grid')
 export class Columns extends LitElement
 {
-  @property({attribute: 'layout', type: String, reflect: true}) layout: string = "";
-  @property({attribute: 'expanded', type: Boolean, reflect: true}) expanded: boolean = false;
-  @property({attribute: 'min', type: Number, reflect: true}) min;
-
-  layoutClass = "";
-
   static styles = unsafeCSS(styles);
 
-  connectedCallback()
-  {
-    super.connectedCallback();
-    new ResizeObserver(this.resize).observe(this);
-  }
-
-  resize = (e) =>
-  {
-    this.expanded = e[0].contentRect.width > 800;
-  };
+  @property({type: String}) layout: string = "";
 
   render()
   {
+    const elements = this.querySelectorAll(':scope > *');
+    const count = elements.length;
 
-    if(this.min > 0)
-    {
-      this.style.setProperty('--col-min', this.min + "px");
-    }
-
-    const c1c = this.querySelectorAll('[slot="c1"]').length > 0 ? 1 : 0;
-    const c2c = this.querySelectorAll('[slot="c2"]').length > 0 ? 1 : 0;
-    const c3c = this.querySelectorAll('[slot="c3"]').length > 0 ? 1 : 0;
-    const c4c = this.querySelectorAll('[slot="c4"]').length > 0 ? 1 : 0;
-
-    const slotCount = c1c + c2c + c3c + c4c;
     if(this.layout.length < 1)
     {
-
-      switch(slotCount)
+      switch(count)
       {
         case 1:
-          this.layout += "4";
+          this.layout = "4";
           break;
         case 2:
-          this.layout += "2,2";
+          this.layout = "2,2";
           break;
         case 3:
-          this.layout += "1,1,2";
+          this.layout = "1,1,2";
           break;
         case 4:
-          this.layout += "1,1,1,1";
+        default:
+          this.layout = "1,1,1,1";
           break;
       }
     }
 
-    const cols = this.layout.replace(/,/g, '').split("");
-    this.layoutClass = "lo-" + this.layout.replace(/,/g, '');
-    let span = 0;
-    const slots = ['', '', '', ''];
-
-    for(let i = 0; i < cols.length; i++)
+    const layout = this.layout.split(',');
+    elements.forEach((element, index) =>
     {
-      span = span + parseInt(cols[i]);
-      if(span <= 4)
+      if(index >= layout.length)
       {
-        this.layoutClass += " c" + (i + 1);
-        slots[i] = "s" + cols[i];
+        element.setAttribute('col', '4');
+        return;
       }
-    }
+      element.setAttribute('col', `${layout[index]}`);
+    });
 
-    const c1 = c1c ? html`
-      <div class="${slots[0]}">
-        <slot name="c1"></slot>
-      </div>` : null;
-    const c2 = c2c ? html`
-      <div class="${slots[1]}">
-        <slot name="c2"></slot>
-      </div>` : null;
-    const c3 = c3c ? html`
-      <div class="${slots[2]}">
-        <slot name="c3"></slot>
-      </div>` : null;
-    const c4 = c4c ? html`
-      <div class="${slots[3]}">
-        <slot name="c4"></slot>
-      </div>` : null;
-
+    this.classList.add('grid');
 
     return html`
-      <div class="${this.layoutClass}">
-        ${c1} ${c2} ${c3} ${c4}
-      </div>
+      <slot></slot>
     `;
   }
 }
-
-

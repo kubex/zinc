2015d46e60596cbc67a637ad99378a45a2e1e59a
Brooke Bryan <brooke@bajb.net>
2023-09-28 23:08:34 +0100

    Remove loading image

--- src/Container/container.ts
+++ src/Container/container.ts
@@ -128,6 +128,11 @@ export class Container extends ZincElement
 
   set innerHTML(data)
   {
+    if(this.shadowRoot === null)
+    {
+      return;
+    }
+
     const slot = this.shadowRoot.getElementById('slot');
     if(slot)
     {
@@ -188,8 +193,6 @@ export class Container extends ZincElement
         <div class="page-loading">
           <div>
             <p>Loading, please wait...</p>
-            <img alt="Loading"
-                 src="data:image/gif;base64,R0lGODlhKwALAPEAAP///wA1aYKbtQA1aSH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAKwALAAACMoSOCMuW2diD88UKG95W88uF4DaGWFmhZid93pq+pwxnLUnXh8ou+sSz+T64oCAyTBUAACH5BAkKAAAALAAAAAArAAsAAAI9xI4IyyAPYWOxmoTHrHzzmGHe94xkmJifyqFKQ0pwLLgHa82xrekkDrIBZRQab1jyfY7KTtPimixiUsevAAAh+QQJCgAAACwAAAAAKwALAAACPYSOCMswD2FjqZpqW9xv4g8KE7d54XmMpNSgqLoOpgvC60xjNonnyc7p+VKamKw1zDCMR8rp8pksYlKorgAAIfkECQoAAAAsAAAAACsACwAAAkCEjgjLltnYmJS6Bxt+sfq5ZUyoNJ9HHlEqdCfFrqn7DrE2m7Wdj/2y45FkQ13t5itKdshFExC8YCLOEBX6AhQAADsAAAAAAAAAAAA="/>
           </div>
         </div>
       </slot>

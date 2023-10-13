64c52a438d601a5fb60878b2a6ffedf1e0bc125e
Brooke Bryan <brooke@bajb.net>
2023-10-13 11:37:17 +0100

    Update stat

--- src/StatsTile/stat.ts
+++ src/StatsTile/stat.ts
@@ -9,11 +9,12 @@ export class Stat extends ZincElement
 {
   static styles = unsafeCSS(styles);
 
-  @property({type: String}) caption;
-  @property({type: String}) description;
-  @property({type: String}) amount;
-  @property({type: String}) previous;
-  @property({type: String}) currency;
+  @property({type: String}) caption = '';
+  @property({type: String}) description = '';
+  @property({type: String}) amount = '0';
+  @property({type: String}) previous = '0';
+  @property({type: String}) currency = '';
+  @property({type: Boolean, attribute: 'show-delta'}) showDelta = false;
 
   calcPercentageDifference()
   {
@@ -28,32 +29,40 @@ export class Stat extends ZincElement
 
   getCurrentAmount()
   {
+    if(this.currency == "")
+    {
+      return this.amount;
+    }
     // convert string to money
     const amount = parseFloat(this.amount);
-    const currency = this.currency || '£';
+    const currency = this.currency;
 
     return `${currency}${amount.toFixed(2)}`;
   }
 
   render()
   {
-    const diff = this.calcPercentageDifference();
-    let diffClass = 'neutral';
-    let diffIcon = '~';
-    if(diff > 0)
+    let diffDisplay = null;
+    if(this.showDelta)
     {
-      diffClass = 'positive';
-      diffIcon = '+';
-    }
+      const diff = this.calcPercentageDifference();
+      let diffClass = 'neutral';
+      let diffIcon = '~';
+      if(diff > 0)
+      {
+        diffClass = 'positive';
+        diffIcon = '+';
+      }
 
-    if(diff < 0)
-    {
-      diffClass = 'negative';
-      diffIcon = '-';
-    }
+      if(diff < 0)
+      {
+        diffClass = 'negative';
+        diffIcon = '-';
+      }
 
-    const diffDisplay = html`<p class="diff "><span class="${diffClass}">${diffIcon}${diff}%</span> on Previous Period
-    </p>`;
+      diffDisplay = html`<p class="diff "><span class="${diffClass}">${diffIcon}${diff}%</span> on Previous Period
+      </p>`;
+    }
 
     return html`
       <div>

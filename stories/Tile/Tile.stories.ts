be933725ecb364d7049d5ebf449c09f58fcacd1f
Kyle Essex <kylegavinessex@hotmail.com>
2024-03-27 18:08:16 +0000

    Add Tile story Include package-lock as an update breaks storybook

---
+++ stories/Tile/Tile.stories.ts
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+import '../../src/Tile';
+import '../../src/Chip';
+import '../../src/Icon';
+
+const meta: Meta = {
+  component: 'zn-tile',
+  title: 'Elements/Tile',
+  tags: ['elements', 'tile'],
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Text: Story = {
+  render: ({caption, description}) => html`
+    <zn-tile caption="${caption}" description="${description}"></zn-tile>`,
+  args: {
+    caption: 'Mark Crayon',
+    description: 'Manager',
+  },
+};
+
+
+export const Default: Story = {
+  render: ({caption, description}) => html`
+    <zn-tile caption="${caption}" description="${description}">
+      <zn-icon slot="primary" round size="60" src="https://i.pravatar.cc/60?img=20"></zn-icon>
+      <div slot="status">
+        <zn-chip>EN</zn-chip>
+        <zn-chip>DE</zn-chip>
+      </div>
+    </zn-tile>`,
+  args: {
+    caption: 'Mark Crayon',
+    description: 'Manager',
+  },
+};

959219c0d0324f710991d5c75f80e53b8b92cf79
Kyle Essex <kylegavinessex@hotmail.com>
2024-03-28 09:43:41 +0000

    Add more examples of Tile

--- stories/Tile/Tile.stories.ts
+++ stories/Tile/Tile.stories.ts
@@ -14,8 +14,8 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj;
 
-export const Text: Story = {
-  render: ({caption, description}) => html`
+export const Default: Story = {
+  render: ({ caption, description }) => html`
     <zn-tile caption="${caption}" description="${description}"></zn-tile>`,
   args: {
     caption: 'Mark Crayon',
@@ -24,14 +24,36 @@ export const Text: Story = {
 };
 
 
-export const Default: Story = {
-  render: ({caption, description}) => html`
+export const PrimaryAndStatus: Story = {
+  render: ({ caption, description }) => html`
     <zn-tile caption="${caption}" description="${description}">
       <zn-icon slot="primary" round size="60" src="https://i.pravatar.cc/60?img=20"></zn-icon>
       <div slot="status">
         <zn-chip>EN</zn-chip>
         <zn-chip>DE</zn-chip>
       </div>
+    </zn-tile>`,
+  args: {
+    caption: 'Mark Crayon',
+    description: 'Manager',
+  },
+};
+
+export const Status: Story = {
+  render: ({ caption, description }) => html`
+    <zn-tile caption="${caption}" description="${description}">
+      <zn-chip slot="status" success>paid</zn-chip>
+    </zn-tile>`,
+  args: {
+    caption: 'May 17th 2023 — $12.99',
+    description: 'Manager',
+  },
+};
+
+export const Primary: Story = {
+  render: ({ caption, description }) => html`
+    <zn-tile caption="${caption}" description="${description}">
+      <zn-icon slot="primary" round size="60" src="https://i.pravatar.cc/60?img=20"></zn-icon>
     </zn-tile>`,
   args: {
     caption: 'Mark Crayon',

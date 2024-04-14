05e57e8ec130f988dd6621475b8573f0737b3f67
Brooke Bryan <brooke@bajb.net>
2024-04-14 21:14:10 +0100

    Stop dropping the header on theme toggle

--- src/Pane/pane.ts
+++ src/Pane/pane.ts
@@ -1,25 +1,32 @@
-import { html, unsafeCSS } from "lit";
-import { customElement } from 'lit/decorators.js';
+import {html, unsafeCSS} from "lit";
+import {customElement} from 'lit/decorators.js';
 
 import styles from './index.scss';
-import { ZincElement } from "../zinc";
+import {ZincElement} from "../zinc";
 
 @customElement('zn-pane')
 export class Pane extends ZincElement
 {
   static styles = unsafeCSS(styles);
 
+  protected _header: HTMLElement;
+
+  connectedCallback()
+  {
+    super.connectedCallback();
+    this._header = this.querySelector("zn-header");
+  }
+
   render()
   {
-    const header = this.querySelector("zn-header");
 
-    if(header)
+    if(this._header)
     {
       this.classList.add("with-header");
     }
 
     return html`
-      ${header}
+      ${this._header}
       <div class="pane__content">
         <slot></slot>
       </div>`;

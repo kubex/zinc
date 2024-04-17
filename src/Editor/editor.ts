d095801cfefd24b7325f3f6b71cbbf3996298f06
Kyle Essex <kylegavinessex@hotmail.com>
2024-04-17 11:14:07 +0100

    Add clear formatting button

--- src/Editor/editor.ts
+++ src/Editor/editor.ts
@@ -93,6 +93,11 @@ export class Editor extends LitElement
     <polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon>
     <path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path>
   </svg>`;
+    icons["remove-formatting"] = `<svg viewbox="0 0 18 18">
+    <rect class="ql-stroke" height="12" width="12" x="3" y="3"></rect>
+    <line class="ql-stroke" x1="3" x2="15" y1="3" y2="15"></line>
+    <line class="ql-stroke" x1="3" x2="15" y1="15" y2="3"></line>
+  </svg>`;
 
     const quill = new Quill(this.editor, {
       modules: {
@@ -102,6 +107,7 @@ export class Editor extends LitElement
             ['bold', 'italic', 'underline', 'strike'],
             ['undo', 'redo'],
             [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+            ['remove-formatting'],
           ],
           handlers: {
             'redo': () =>
@@ -111,6 +117,14 @@ export class Editor extends LitElement
             'undo': () =>
             {
               this.quillElement.history.undo();
+            },
+            'remove-formatting': () =>
+            {
+              const range = this.quillElement.getSelection();
+              this.quillElement.formatText(range.index, range.length, 'bold', false);
+              this.quillElement.formatText(range.index, range.length, 'italic', false);
+              this.quillElement.formatText(range.index, range.length, 'underline', false);
+              this.quillElement.formatText(range.index, range.length, 'strike', false);
             }
           }
         },

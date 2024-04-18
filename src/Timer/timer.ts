160be0301aa9897a14c16cf383bcacb9c31e49e3
Kyle Essex <kylegavinessex@hotmail.com>
2024-04-18 11:44:22 +0100

    Timer fixes

--- src/Timer/timer.ts
+++ src/Timer/timer.ts
@@ -22,37 +22,47 @@ export class Timer extends LitElement
     const time = new Date(parseInt(this.timestamp) * 1000);
     const diff = (new Date()).getTime() - time.getTime();
 
-    let hours = Math.floor(diff / 1000 / 60 / 60);
-    let minutes = Math.floor(diff / 1000 / 60) - (hours * 60);
-    let seconds = Math.floor(diff / 1000) - (minutes * 60) - (hours * 60 * 60);
 
-    let displayMinutes: number | string = minutes;
-    let displaySeconds: number | string = seconds;
-    let displayHours: number | string = hours;
-    if(minutes < 10)
-    {
-      displayMinutes = `0${minutes}`;
-    }
+    const months = Math.floor(diff / 1000 / 60 / 60 / 24 / 30);
+    const weeks = Math.floor(diff / 1000 / 60 / 60 / 24 / 7);
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
 
-    if(seconds < 10)
-    {
-      displaySeconds = `0${seconds}`;
-    }
 
-    if(hours < 10)
-    {
-      displayHours = `0${hours}`;
-    }
+    // limit timer to always HH:MM or MM:SS or DD:HH
+    let displayMonths = months < 10 && months >= 0 ? `0${months}` : months;
+    let displayWeeks = weeks < 10 && weeks >= 0 ? `0${weeks}` : weeks;
+    let displayDays = days < 10 && days >= 0 ? `0${days}` : days;
+    let displayMinutes = minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes;
+    let displaySeconds = seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds;
+    let displayHours = hours < 10 && hours >= 0 ? `0${hours}` : hours;
 
-    let timeString = '';
-    if(hours > 0)
+    displayMonths = months > 0 ? `${displayMonths}M` : 0;
+    displayWeeks = weeks > 0 ? `${displayWeeks}w` : 0;
+    displayDays = days > 0 ? `${displayDays}d` : 0;
+    displayHours = hours > 0 ? `${displayHours}` : 0;
+    displayMinutes = minutes > 0 ? `${displayMinutes}` : 0;
+    displaySeconds = seconds > 0 ? `${displaySeconds}` : 0;
+
+    const array = [displayMonths, displayWeeks, displayDays, displayHours, displayMinutes, displaySeconds];
+
+    let count = 0;
+    let prev = false;
+    const times = [];
+    array.forEach((item, _) =>
     {
-      timeString = `${displayHours}:${displayMinutes}:${displaySeconds}`;
-    }
-    else
-    {
-      timeString = `${displayMinutes}:${displaySeconds}`;
-    }
+      if(((item === 0 || item === '00') && !prev) || count === 2)
+      {
+        return;
+      }
+
+      times.push(item);
+      prev = true;
+      count++;
+    });
+
 
     // increment every second
     setTimeout(() =>
@@ -63,14 +73,8 @@ export class Timer extends LitElement
 
     return html`
       <div class="last-message upper-limit-reached">
-        <span class="time">${timeString}</span>
+        <span class="time">${times.join(':')}</span>
         <span class="last-message-indicator"></span>
-      </div>`;
-
-
-    return html`
-      <div class="last-message">
-        <span class="time">${timeString}</span>
       </div>`;
   }
 

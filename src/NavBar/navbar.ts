fe6b480c51e79011657e599cfade838d7f1cb117
Brooke Bryan <brooke@bajb.net>
2024-04-18 07:46:10 +0100

    Allow suffixed items

--- src/NavBar/navbar.ts
+++ src/NavBar/navbar.ts
@@ -8,23 +8,32 @@ import styles from './index.scss';
 export class NavBar extends ZincElement
 {
 
+  private _preItems: NodeListOf<Element>;
+  private _postItems: NodeListOf<Element>;
   @property({attribute: 'navigation', type: Array}) navigation = [];
   @property({attribute: 'full-width', type: Boolean, reflect: true}) fullWidth: boolean;
 
   static styles = unsafeCSS(styles);
 
+  connectedCallback()
+  {
+    super.connectedCallback();
+    this._preItems = this.querySelectorAll('li:not([suffix])');
+    this._postItems = this.querySelectorAll('li[suffix]');
+  }
+
   render()
   {
     return html`
       <ul>
-        <slot></slot>
+        ${this._preItems}
         ${this.navigation.map((item, index) =>
         {
           const activeClass = item.active ? 'active' : '';
           return html`
             <li class="${activeClass}" tab-uri="${item.path}">${item.title}</li>`;
         })}
-        <slot name="last"></slot>
+        ${this._postItems}
       </ul>`;
   }
 }

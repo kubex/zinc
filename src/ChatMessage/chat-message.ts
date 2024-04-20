e51125d423e02e9ed54bc49fafc274a769cf0714
Kyle Essex <kylegavinessex@hotmail.com>
2024-04-20 13:35:50 +0100

    Chat messages

--- src/ChatMessage/chat-message.ts
+++ src/ChatMessage/chat-message.ts
@@ -2,32 +2,61 @@ import { html, LitElement, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import styles from './index.scss';
+import { cleanHTML } from "./clean-message";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+
+
+type ChatMessageActionType = ""
+  | 'connected.agent'
+  | 'attachment.added'
+  | "multi.answer"
+  | "transfer"
+  | "ended"
+  | "error"
+  | "customer.ended";
+
 
 @customElement('zn-chat-message')
 export class ChatMessage extends LitElement
 {
   static styles = unsafeCSS(styles);
 
-  @property({ type: String })
-  private sender = '';
-
-  @property({ type: String })
-  private time = '';
-
-  @property({ type: String })
-  private type = '';
+  @property({ type: String }) sender = '';
+  @property({ type: String }) message = '';
+  @property({ type: String }) time = '';
+  @property({ type: String }) actionType: ChatMessageActionType = '';
+  @property({ type: String }) customerInitiated = "0";
 
   render()
   {
+    if(!this._displayMessage())
+    {
+      return html``;
+    }
+
+    if(this.actionType === 'multi.answer')
+    {
+      return this._displayMultiAnswer();
+    }
+
     return html`
       <div class="wrapper ${this.sender ? 'message-' + this.sender : ''}">
         <div class="message">
-          <slot></slot>
+          ${this._prepareMessageContent()}
         </div>
-        <div class="time">
-          ${this.sender} ${this.time ? this._getSentTime() : null}
+        <div class="int-msg-d">
+          ${this._getAuthor()}
+          ${this._getTime()}
         </div>
       </div>`;
+  }
+
+  private _displayMessage()
+  {
+    const types = ["", 'connected.agent', 'attachment.added', "multi.answer", "transfer", "ended", "error", "customer.ended"];
+    const type = types.indexOf(this.actionType);
+
+    return type > -1;
   }
 
   private _getSentTime()
@@ -45,4 +74,43 @@ export class ChatMessage extends LitElement
     }
 
   }
+
+  private _displayMultiAnswer()
+  {
+    return html`
+      <div class="wrapper ${this.sender ? 'message-' + this.sender : ''}">
+        <div class="message">
+          ${this._prepareMessageContent()}
+        </div>
+        <div class="int-msg-d">
+          ${this._getAuthor()}
+          ${this._getTime()}
+        </div>
+      </div>`;
+  }
+
+  private _prepareMessageContent()
+  {
+    let content = cleanHTML(this.message);
+
+    content = content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    content = content.replace(/\b(((https?|ftp|dict):\/\/|www\.)[^'">\s]+\.[^'">\s]+)(?=\s|$)(?!["<>])/g, '<a href="$1" target="_blank">$1</a>');
+    content = unsafeHTML(content) as string;
+    return content;
+  }
+
+  private _getAuthor()
+  {
+    return html`<span class="int-msg-who">
+      ${this.sender ? this.sender : (this.customerInitiated === "0" ? 'Customer' : 'You')}
+    </span>`;
+  }
+
+  private _getTime()
+  {
+    return html`<span class="int-msg-time">
+      ${this.time ? this._getSentTime() : null}
+    </span>`;
+  }
+
 }

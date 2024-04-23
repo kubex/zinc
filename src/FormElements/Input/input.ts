232d05c791531531c8d367df5db81261077b8b9e
Kyle Essex <kylegavinessex@hotmail.com>
2024-04-23 11:26:02 +0100

    Update input to use formcontrolcontroller

--- src/FormElements/Input/input.ts
+++ src/FormElements/Input/input.ts
@@ -1,20 +1,73 @@
-import {html, LitElement, unsafeCSS} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { ZincElement, ZincFormControl } from "../../zinc-element";
+import { FormControlController } from "../../form";
+
+import { html, unsafeCSS } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+
+import { PropertyValues } from "@lit/reactive-element";
 
 import styles from './index.scss';
 
 @customElement('zn-input')
-export class Input extends LitElement
+export class Input extends ZincElement implements ZincFormControl
 {
-  @property({type: String, reflect: true}) for;
-  @property({type: String, reflect: true}) label;
-  @property({type: String, reflect: true}) class;
-  @property({type: String, reflect: true}) advice;
-  @property({type: String, reflect: true}) summary;
-  @property({type: String, reflect: true}) prefix;
-  @property({type: String, reflect: true}) suffix;
-
   static styles = unsafeCSS(styles);
+
+  private readonly formControlController = new FormControlController(this, {});
+
+  @query('input') input: HTMLInputElement;
+
+  @property({ type: String, reflect: true }) for;
+  @property({ type: String, reflect: true }) label;
+  @property({ type: String, reflect: true }) class;
+  @property({ type: String, reflect: true }) advice;
+  @property({ type: String, reflect: true }) summary;
+  @property({ type: String, reflect: true }) prefix;
+  @property({ type: String, reflect: true }) suffix;
+
+  @property() name: string = "";
+  @property() value: string;
+
+  get validity()
+  {
+    return this.input.validity;
+  }
+
+  get validationMessage()
+  {
+    return this.input.validationMessage;
+  }
+
+  protected firstUpdated(_changedProperties: PropertyValues)
+  {
+    return this.formControlController.updateValidity();
+  }
+
+  checkValidity(): boolean
+  {
+    return this.input.checkValidity();
+  }
+
+  getForm(): HTMLFormElement | null
+  {
+    return this.formControlController.getForm();
+  }
+
+  reportValidity(): boolean
+  {
+    return this.input.reportValidity();
+  }
+
+  setCustomValidity(message: string): void
+  {
+    this.input.setCustomValidity(message);
+    this.formControlController.updateValidity();
+  }
+
+  private handleClick()
+  {
+    this.querySelector('input').focus();
+  }
 
   render()
   {
@@ -22,16 +75,12 @@ export class Input extends LitElement
       <label for="${this.for}">${this.label}</label>
       ${this.summary}
       <div class="wrap">
-        ${this.prefix ? html`<span @click="${this._getFocus}" class="prefix">${this.prefix}</span>` : ''}
+        ${this.prefix ? html`<span @click="${this.handleClick}" class="prefix">${this.prefix}</span>` : ''}
         <slot></slot>
-        ${this.suffix ? html`<span @click="${this._getFocus}" class="suffix">${this.suffix}</span>` : ''}
+        ${this.suffix ? html`<span @click="${this.handleClick}" class="suffix">${this.suffix}</span>` : ''}
       </div>
       <span class="advice">${this.advice}</span>
     `;
   }
-
-  _getFocus()
-  {
-    this.querySelector('input').focus();
-  }
 }
+

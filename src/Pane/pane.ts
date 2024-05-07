633bc664e9ad18b4bc28c890c5e6c22d5c927be9
Kyle Essex <kylegavinessex@hotmail.com>
2024-05-07 14:06:30 +0100

    Roll back pane

--- src/Pane/pane.ts
+++ src/Pane/pane.ts
@@ -1,9 +1,8 @@
-import {html, unsafeCSS} from "lit";
-import {property, customElement} from 'lit/decorators.js';
+import { html, unsafeCSS } from "lit";
+import { customElement } from 'lit/decorators.js';
 
 import styles from './index.scss';
-import {ZincElement} from "../zinc-element";
-import {Element} from "chart.js";
+import { ZincElement } from "../zinc-element";
 
 @customElement('zn-pane')
 export class Pane extends ZincElement
@@ -17,11 +16,6 @@ export class Pane extends ZincElement
     this._header = this.querySelector("zn-header");
   }
 
-  createRenderRoot()
-  {
-    return this;
-  }
-
   render()
   {
     if(this._header)
@@ -32,14 +26,7 @@ export class Pane extends ZincElement
     return html`
       ${this._header}
       <div class="pane__content">
-        ${Object.entries(this.childNodes).map(([key, val]) =>
-        {
-          if(val == this._header)
-          {
-            return;
-          }
-          return html`${val}`;
-        })}
+        <slot></slot>
       </div>`;
   }
 

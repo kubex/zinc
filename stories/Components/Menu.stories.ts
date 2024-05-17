7973a2f54136be6ba2c6100c97f6ec15f1be5ea7
Jleagle <jimeagle@gmail.com>
2024-05-17 17:15:42 +0100

    Menu story

---
+++ stories/Components/Menu.stories.ts
@@ -0,0 +1,34 @@
+import {html} from "lit";
+import {Meta, StoryObj} from "@storybook/web-components";
+
+import '../../src/Menu';
+
+const meta: Meta = {
+  component: 'zn-menu',
+  title: 'Components/Menu',
+  tags: ['components', 'menu'],
+};
+
+export default meta;
+type Story = StoryObj;
+
+
+export const Default: Story = {
+  render: ({actions}) =>
+  {
+    return html`
+      <zn-menu actions="${JSON.stringify(actions)}"></zn-menu>`;
+  },
+  args: {
+    actions: [
+      {
+        "title": "Home",
+        "path": "#"
+      },
+      {
+        "title": "Settings",
+        "path": "/settings"
+      }
+    ],
+  }
+};

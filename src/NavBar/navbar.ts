e97fb4440c0b5924ca7b10b4782dc03bdf4caf55
Kyle Essex <kylegavinessex@hotmail.com>
2024-06-14 16:07:56 +0100

    Updates

--- src/NavBar/navbar.ts
+++ src/NavBar/navbar.ts
@@ -1,6 +1,6 @@
-import {html, TemplateResult, unsafeCSS} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {ZincElement} from "../zinc-element";
+import { html, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { ZincElement } from "../zinc-element";
 
 
 import styles from './index.scss?inline';
@@ -11,10 +11,10 @@ export class NavBar extends ZincElement
 
   private _preItems: NodeListOf<Element>;
   private _postItems: NodeListOf<Element>;
-  @property({attribute: 'navigation', type: Array}) navigation = [];
-  @property({attribute: 'full-width', type: Boolean, reflect: true}) fullWidth: boolean;
-  @property({attribute: 'icon-bar', type: Boolean, reflect: true}) iconBar: boolean;
-  @property({attribute: 'hide-one', type: Boolean, reflect: true}) hideOne: boolean;
+  @property({ attribute: 'navigation', type: Array }) navigation = [];
+  @property({ attribute: 'full-width', type: Boolean, reflect: true }) fullWidth: boolean;
+  @property({ attribute: 'icon-bar', type: Boolean, reflect: true }) iconBar: boolean;
+  @property({ attribute: 'hide-one', type: Boolean, reflect: true }) hideOne: boolean;
 
   static styles = unsafeCSS(styles);
 
@@ -27,7 +27,11 @@ export class NavBar extends ZincElement
 
   render()
   {
-    let itemCount = this.navigation.length + this._preItems.length + this._postItems.length;
+    if(!this.navigation)
+    {
+      this.navigation = [];
+    }
+    const itemCount = this.navigation?.length + this._preItems?.length + this._postItems?.length;
     if(itemCount < 2 && this.hideOne)
     {
       this.style.display = 'none';

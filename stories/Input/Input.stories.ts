2aaa4dd032c362edab4bac0084026d80a73c4291
Kyle Essex <kylegavinessex@hotmail.com>
2024-08-02 15:42:07 +0100

    Fix inputs

--- stories/Input/Input.stories.ts
+++ stories/Input/Input.stories.ts
@@ -32,6 +32,42 @@ export const Default: Story = {
   },
 };
 
+export const Normal: Story = {
+  render: ({ for: forAttr, label, class: classAttr, advice, summary}) =>
+  {
+    return html`
+      <zn-input for="${forAttr}" label="${label}" class="${classAttr}" advice="${advice}" summary="${summary}">
+        <input type="text" id="${forAttr}" name="${forAttr}"/>
+      </zn-input>`;
+  },
+  args: {
+    for: 'text',
+    label: 'Hello World',
+    class: '',
+    advice: 'Some advice',
+    summary: 'The summary',
+  },
+};
+
+export const Suffix: Story = {
+  render: ({ for: forAttr, label, class: classAttr, advice, summary, suffix }) =>
+  {
+    return html`
+      <zn-input for="${forAttr}" label="${label}" class="${classAttr}" advice="${advice}" summary="${summary}"
+                suffix="${suffix}">
+        <input type="text" id="${forAttr}" name="${forAttr}"/>
+      </zn-input>`;
+  },
+  args: {
+    for: 'text',
+    label: 'Hello World',
+    class: '',
+    advice: 'Some advice',
+    summary: 'The summary',
+    suffix: 'Suffix',
+  },
+};
+
 export const TextArea: Story = {
   render: ({ for: forAttr, label, class: classAttr, advice, summary, prefix, suffix }) =>
   {

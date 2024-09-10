4d55e231eed603131c4d7e041fbbe8dd7ee71b95
Kyle Essex <kylegavinessex@hotmail.com>
2024-09-10 11:57:00 +0100

    Improve Colours

--- src/Charts/DataChart/data-chart.ts
+++ src/Charts/DataChart/data-chart.ts
@@ -35,6 +35,7 @@ export class DataChart extends EmptyZincFormElement
           sizeOffset: 2
         }
       },
+      colors: ["#8967ef", "#6483F2", "#29bab5", "#3F51B5", "#9C27B0", "#ff6c9c", "#6836F5", "#47D6D6"],
       legend: {
         position: 'top',
         horizontalAlign: 'right',

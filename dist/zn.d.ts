abcba3569f64952c02d4a2893c539792f9a87027
MrEssex <MrEssex@users.noreply.github.com>
2025-03-21 10:37:18 +0000

    deploy: 7282aafdd568c30750327083a3c14b1508236ecd

--- dist/zn.d.ts
+++ dist/zn.d.ts
@@ -2099,7 +2099,8 @@ declare module "events/zn-menu-select" {
     }
 }
 declare module "components/navbar/navbar.component" {
-    import { type CSSResultGroup, PropertyValues } from 'lit';
+    import type { PropertyValues } from 'lit';
+    import { type CSSResultGroup } from 'lit';
     import ZincElement from "internal/zinc-element";
     /**
      * @summary Short summary of the component's intended use.

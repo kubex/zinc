aa3a74ccd983bd3e5fb44fccd91a7b267c953f44
Alan Jones <alan.jones@chargehive.com>
2025-03-27 13:38:30 +0000

    Note styling & color WIP

--- src/components/note/note.component.ts
+++ src/components/note/note.component.ts
@@ -1,4 +1,6 @@
-import { type CSSResultGroup, html, unsafeCSS } from 'lit';
+import {classMap} from "lit/directives/class-map.js";
+import {type CSSResultGroup, html, unsafeCSS} from 'lit';
+import {property} from "lit/decorators.js";
 import ZincElement from '../../internal/zinc-element';
 
 import styles from './note.scss';
@@ -23,14 +25,27 @@ import styles from './note.scss';
 export default class ZnNote extends ZincElement {
   static styles: CSSResultGroup = unsafeCSS(styles);
 
+  @property({reflect: true}) color: 'red' | 'blue' | 'orange' | 'yellow' | 'indigo' | 'violet' | 'green' | 'pink' | 'gray' | '' = '';
+
   protected render(): unknown {
     return html`
-      <div>
-        <div class="header">
-          <slot class="caption" name="caption"></slot>
-          <slot class="date" name="date"></slot>
+      <div class="${classMap({
+        'note': true,
+        'note--red': this.color === 'red',
+        'note--blue': this.color === 'blue',
+        'note--orange': this.color === 'orange',
+        'note--yellow': this.color === 'yellow',
+        'note--indigo': this.color === 'indigo',
+        'note--violet': this.color === 'violet',
+        'note--green': this.color === 'green',
+        'note--pink': this.color === 'pink',
+        'note--gray': this.color === 'gray',
+      })}">
+        <div class="note__header">
+          <slot class="note__header__caption" name="caption"></slot>
+          <slot class="note__header__date" name="date"></slot>
         </div>
-        <div class="body">
+        <div class="note__body">
           <slot></slot>
         </div>
       </div>

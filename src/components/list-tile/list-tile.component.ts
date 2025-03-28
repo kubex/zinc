15da0cd63193788e56e616664e471181345edcf1
Kyle Essex <kylegavinessex@hotmail.com>
2025-03-28 15:01:41 +0000

    Support data uri and data-target

--- src/components/list-tile/list-tile.component.ts
+++ src/components/list-tile/list-tile.component.ts
@@ -1,12 +1,12 @@
 import {classMap} from "lit/directives/class-map.js";
 import {type CSSResultGroup, unsafeCSS} from 'lit';
 import {HasSlotController} from "../../internal/slot";
-import {html} from "lit/static-html.js";
+import {html, literal} from "lit/static-html.js";
 import {property} from 'lit/decorators.js';
+import {ifDefined} from "lit/directives/if-defined.js";
 import ZincElement from '../../internal/zinc-element';
 
 import styles from './list-tile.scss';
-import {ifDefined} from "lit/directives/if-defined.js";
 
 /**
  * @summary Short summary of the component's intended use.
@@ -31,36 +31,32 @@ export default class ZnListTile extends ZincElement {
   private readonly hasSlotController = new HasSlotController(this, '[default]');
 
   @property({attribute: 'caption'}) caption: string;
+
   @property({attribute: 'description'}) description: string;
-  @property({attribute: 'link'}) href: string;
+
+  @property({attribute: 'href'}) href: string;
+
   @property({attribute: 'data-target'}) dataTarget: string;
 
-  private clickHandler = (e: MouseEvent) => {
-    e.preventDefault();
+  @property({attribute: 'data-uri'}) dataUri: string;
 
-    if (e.target && (e.target as HTMLElement).tagName === 'ZN-BUTTON') {
-      console.log('button clicked');
-      return;
-    }
-
-    this.dispatchEvent(new CustomEvent('zn-click', {bubbles: true, composed: true}));
-
-    const anchor = this.shadowRoot?.querySelector<HTMLAnchorElement>('#anchor-trigger');
-    if (anchor) {
-      anchor.click();
-    }
+  private _isLink() {
+    return this.href || this.dataUri;
   }
 
   render() {
+    const isLink = this._isLink();
+    const tag = isLink ? literal`a` : literal`div`;
+
     return html`
-      ${this.href ? html`<a id="anchor-trigger" href="${this.href}" style="display: none"></a>` : ''}
-      <div
-        @click="${this.href ? this.clickHandler : undefined}"
+      <${tag}
+        href="${ifDefined(this.href)}"
+        data-uri="${ifDefined(this.dataUri)}"
         data-target="${ifDefined(this.dataTarget)}"
         class="${classMap({
           tile: true,
           'tile--has-image': this.hasSlotController.test('image'),
-          'tile--has-href': this.href
+          'tile--has-href': isLink
         })}">
         <div class="tile__left">
           <slot name="image" part="image" class="tile__image"></slot>
@@ -73,6 +69,6 @@ export default class ZnListTile extends ZincElement {
           <slot name="properties" part="properties" class="tile__properties"></slot>
           <slot name="actions" part="actions" class="tile__actions"></slot>
         </div>
-      </div>`;
+        </div>`;
   }
 }

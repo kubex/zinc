91b8dd691fe903070df76047a3e1689ba9451edc
MrEssex <MrEssex@users.noreply.github.com>
2025-04-03 10:48:42 +0000

    deploy: 8b806c0011d0a5af469a4dd8f7bfc2024bae8d4e

--- dist/zn.d.ts
+++ dist/zn.d.ts
@@ -1196,6 +1196,12 @@ declare module "utilities/localize" {
         showPassword: string;
         clearEntry: string;
         numOptionsSelected: (num: number) => string;
+        fileButtonText: string;
+        fileButtonTextMultiple: string;
+        folderButtonText: string;
+        folderDragDrop: string;
+        fileDragDrop: string;
+        numFilesSelected: (num: number) => string;
     }
 }
 declare module "components/menu-item/submenu-controller" {
@@ -2049,11 +2055,11 @@ declare module "components/option/index" {
 declare module "components/select/select.component" {
     import { type CSSResultGroup, type TemplateResult } from 'lit';
     import { FormControlController } from "internal/form";
-    import type { ZincFormControl } from "internal/zinc-element";
     import ZincElement from "internal/zinc-element";
     import ZnChip from "components/chip/index";
     import ZnIcon from "components/icon/index";
     import ZnPopup from "components/popup/index";
+    import type { ZincFormControl } from "internal/zinc-element";
     import type ZnOption from "components/option/index";
     /**
      * @summary Short summary of the component's intended use.
@@ -4348,40 +4354,165 @@ declare module "components/radio-group/index" {
         }
     }
 }
-declare module "components/drag-upload/drag-upload.component" {
+declare module "components/file/file.component" {
     import { type CSSResultGroup } from 'lit';
-    import ZincElement, { ZincFormControl } from "internal/zinc-element";
+    import ZincElement, { type ZincFormControl } from "internal/zinc-element";
+    import type ZnButton from "components/button/index";
     /**
-     * @summary Short summary of the component's intended use.
+     * @summary File controls allow selecting an arbitrary number of files for uploading.
      * @documentation https://zinc.style/components/drag-upload
      * @status experimental
      * @since 1.0
      *
-     * @dependency zn-example
+     * @dependency zn-button
+     * @dependency zn-icon
      *
-     * @event zn-event-name - Emitted as an example.
+     * @slot label - The file control's label. Alternatively, you can use the `label` attribute.
+     * @slot help-text - Text that describes how to use the file control.
+     *    Alternatively, you can use the `help-text` attribute.
+     * @slot droparea-icon - Optional droparea icon to use instead of the default.
+     *    Works best with `<zn-icon>`.
+     * @slot trigger - Optional content to be used as trigger instead of the default content.
+     *    Opening the file dialog on click and as well as drag and drop will work for this content.
+     *    Following attributes will no longer work: *label*, *droparea*, *help-text*, *size*,
+     *    *hide-value*. Also if using the disabled attribute, the disabled styling will not be
+     *    applied and must be taken care of yourself.
      *
-     * @slot - The default slot.
-     * @slot example - An example slot.
+     * @event zn-blur - Emitted when the control loses focus.
+     * @event zn-change - Emitted when an alteration to the control's value is committed by the user.
+     * @event zn-error - Emitted when multiple files are selected via drag and drop, without
+     * the `multiple` property being set.
+     * @event zn-focus - Emitted when the control gains focus.
+     * @event zn-input - Emitted when the control receives input.
      *
-     * @csspart base - The component's base wrapper.
+     * @csspart form-control - The form control that wraps the label, input, and help text.
+     * @csspart form-control-label - The label's wrapper.
+     * @csspart form-control-input - The input's wrapper.
+     * @csspart form-control-help-text - The help text's wrapper.
+     * @csspart button-wrapper - The wrapper around the button and text value.
+     * @csspart button - The zn-button acting as a file input.
+     * @csspart button__base - The zn-button's exported `base` part.
+     * @csspart value - The chosen files or placeholder text for the file input.
+     * @csspart droparea - The element wrapping the drop zone.
+     * @csspart droparea-background - The background of the drop zone.
+     * @csspart droparea-icon - The container that wraps the icon for the drop zone.
+     * @csspart droparea-value - The text for the drop zone.
+     * @csspart trigger - The container that wraps the trigger.
      *
-     * @cssproperty --example - An example CSS custom property.
+     * @animation file.iconDrop - The animation to use for the file icon
+     * when a file is dropped
+     * @animation file.text.disappear - The disappear animation to use for the file placeholder text
+     * when a file is dropped
+     * @animation file.text.appear - The appear animation to use for the file placeholder text
+     * when a file is dropped
      */
-    export default class ZnDragUpload extends ZincElement implements ZincFormControl {
+    export default class ZnFile extends ZincElement implements ZincFormControl {
         static styles: CSSResultGroup;
         private readonly formControlController;
-        caption: string;
-        types: string;
-        size: string;
+        private readonly hasSlotController;
+        private readonly localize;
+        private userIsDragging;
+        input: HTMLInputElement;
+        button: ZnButton;
+        dropareaWrapper: HTMLDivElement;
+        dropareaIcon: HTMLSpanElement;
+        inputChosen: HTMLSpanElement;
+        /**
+         * The selected files as a FileList object containing a list of File objects.
+         * The FileList behaves like an array, so you can get the number of selected files
+         * via its length property.
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#getting_information_on_selected_files)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#getting_information_on_selected_files
+         */
+        set files(v: FileList | null);
+        get files(): FileList | null;
+        /** The name of the file control, submitted as a name/value pair with form data. */
         name: string;
-        value: File | null;
-        uploadInput: HTMLInputElement;
+        /**
+         * The current value of the input, submitted as a name/value pair with form data.
+         * Beware that the only valid value when setting a file input is an empty string!
+         */
+        /**
+         * The value of the file control contains a string that represents the path of the selected file.
+         * If multiple files are selected, the value represents the first file in the list.
+         * If no file is selected, the value is an empty string.
+         * Beware that the only valid value when setting a file control is an empty string!
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#value)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#value
+         */
+        set value(v: string);
+        get value(): string;
+        /** The default value of the form control. Primarily used for resetting the form control. */
+        defaultValue: string;
+        /** The file control's size. */
+        size: 'small' | 'medium' | 'large';
+        /** The file control's label. If you need to display HTML, use the `label` slot instead. */
+        label: string;
+        /**
+         * The file control's help text.
+         * If you need to display HTML, use the `help-text` slot instead.
+         */
+        helpText: string;
+        /** Disables the file control. */
+        disabled: boolean;
+        /** Draw the file control as a drop area */
+        droparea: boolean;
+        /**
+         * Comma separated list of supported file types
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept
+         * @example <zn-file accept=".jpg,.jpeg,.png,.gif,text/plain,image/*"></zn-file>
+         */
+        accept: string;
+        /**
+         * Specifies the types of files that the server accepts.
+         * Can be set either to user or environment.
+         * Works only when not using a droparea!
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/capture)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/capture
+         */
+        capture: 'user' | 'environment';
+        /**
+         * Indicates whether the user can select more than one file.
+         * Has no effect if webkitdirectory is set.
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#multiple)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#multiple
+         */
+        multiple: boolean;
+        /**
+         * Indicates that the file control should let the user select directories instead of files.
+         * When a directory is selected, the directory and its entire hierarchy of contents are included
+         * in the set of selected items.
+         * Note: This is a non-standard attribute but is supported in the major browsers.
+         * [see MDN](https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/webkitdirectory)
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/webkitdirectory
+         */
+        webkitdirectory: boolean;
+        /**
+         * By default, form controls are associated with the nearest containing `<form>` element.
+         * This attribute allows you to place the form control outside of a form and associate it
+         * with the form that has this `id`. The form must be in the same document
+         * or shadow root for this to work.
+         */
+        form: string;
+        /** Makes the input a required field. */
+        required: boolean;
+        /** Suppress the value from being displayed in the file control */
+        hideValue: boolean;
         /** Gets the validity state object */
         get validity(): ValidityState;
         /** Gets the validation message */
         get validationMessage(): string;
-        /** Checks the validity but does not show a validation message. Returns `true` when valid and `false` when invalid. */
+        /**
+         * Checks for validity but does not show a validation message.
+         * Returns `true` when valid and `false` when invalid.
+         */
         checkValidity(): boolean;
         /** Gets the associated form, if one exists. */
         getForm(): HTMLFormElement | null;
@@ -4389,21 +4520,43 @@ declare module "components/drag-upload/drag-upload.component" {
         reportValidity(): boolean;
         /** Sets a custom validation message. Pass an empty string to restore validity. */
         setCustomValidity(message: string): void;
-        getHumanTypes(): string;
+        handleDisabledChange(): void;
+        handleValueChange(): Promise<void>;
+        /** Sets focus on the button or droparea. */
+        focus(options?: FocusOptions): void;
+        /** Removes focus from the button or droparea. */
+        blur(): void;
+        private handleInvalid;
+        private handleFiles;
+        private handleTransferItems;
+        private getFilesFromEntry;
+        private handleClick;
+        /** Handles the change event of the native input */
+        private handleChange;
+        private handleDragOver;
+        private handleDragLeave;
+        private handleDrop;
+        /**
+         * Handle the focus of the droparea and emit focus event
+         */
+        private handleFocus;
+        /**
+         * Handle the blur of the droparea and emit blur event
+         */
+        private handleBlur;
+        private renderValue;
+        private renderDroparea;
+        private renderButton;
         render(): import("lit").TemplateResult<1>;
-        handleClearValue: (e: MouseEvent) => void;
-        handleUpload: (e: Event & {
-            target: HTMLInputElement;
-        }) => void;
     }
 }
-declare module "components/drag-upload/index" {
-    import ZnDragUpload from "components/drag-upload/drag-upload.component";
-    export * from "components/drag-upload/drag-upload.component";
-    export default ZnDragUpload;
+declare module "components/file/index" {
+    import ZnFile from "components/file/file.component";
+    export * from "components/file/file.component";
+    export default ZnFile;
     global {
         interface HTMLElementTagNameMap {
-            'zn-drag-upload': ZnDragUpload;
+            'zn-file': ZnFile;
         }
     }
 }
@@ -4699,7 +4852,7 @@ declare module "zinc" {
     export { default as CopyButton } from "components/copy-button/index";
     export { default as DataTable } from "components/data-table/index";
     export { default as Cols } from "components/cols/index";
-    export { default as ConfirmModal } from "components/confirm/index";
+    export { default as Confirm } from "components/confirm/index";
     export { default as Dialog } from "components/dialog/index";
     export { default as Dropdown } from "components/dropdown/index";
     export { default as Menu } from "components/menu/index";
@@ -4707,9 +4860,9 @@ declare module "zinc" {
     export { default as DefinedLabel } from "components/defined-label/index";
     export { default as EmptyState } from "components/empty-state/index";
     export { default as Note } from "components/note/index";
-    export { default as ListTile } from "components/tile/index";
-    export { default as ListTileProperty } from "components/tile-property/index";
-    export { default as DataChart } from "components/chart/index";
+    export { default as Tile } from "components/tile/index";
+    export { default as TileProperty } from "components/tile-property/index";
+    export { default as Chart } from "components/chart/index";
     export { default as SimpleChart } from "components/simple-chart/index";
     export { default as Header } from "components/header/index";
     export { default as Navbar } from "components/navbar/index";
@@ -4745,7 +4898,7 @@ declare module "zinc" {
     export { default as Radio } from "components/radio/index";
     export { default as Rating } from "components/rating/index";
     export { default as RadioGroup } from "components/radio-group/index";
-    export { default as DragUpload } from "components/drag-upload/index";
+    export { default as File } from "components/file/index";
     export { default as CheckboxGroup } from "components/checkbox-group/index";
     export { default as Item } from "components/item/index";
     export { default as DataSelect } from "components/data-select/index";

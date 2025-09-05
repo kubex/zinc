e73142acf68840e5de648403806a75a5385a017a
Alan Jones <alan.jones@chargehive.com>
2025-09-05 11:04:24 +0100

    Check for null form

--- src/components/confirm/confirm.component.ts
+++ src/components/confirm/confirm.component.ts
@@ -164,7 +164,7 @@ export default class ZnConfirm extends ZincElement {
       form.method = 'POST';
 
       this.querySelectorAll('input').forEach((el: HTMLInputElement) => {
-        form.appendChild(el.cloneNode() as Node);
+        form?.appendChild(el.cloneNode() as Node);
       })
 
       this.appendChild(form);

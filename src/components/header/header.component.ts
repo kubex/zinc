abc396d7ab5e33935ad2482cca6bf402dcdb665d
Alan Jones <alan.jones@chargehive.com>
2025-10-07 09:53:30 +0100

    Fix `zn-button` closing tag

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -141,7 +141,7 @@ export default class ZnHeader extends ZincElement {
           ${hasPreviousPath ? html`
             <a href="${this.previousPath}" class="caption__back"
                data-target="${this.previousTarget ? this.previousTarget : ''}">
-              <zn-button size="content" icon="arrow_back" icon-size="24" color="transparent"></zn-buttonicon>
+              <zn-button size="content" icon="arrow_back" icon-size="24" color="transparent"></zn-button>
             </a>` : null}
 
           ${hasDefaultSlot ? html`

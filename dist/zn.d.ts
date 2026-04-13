e30eb3232309298771e544e937c1e5864704d7d3
Jonezzyboy <Jonezzyboy@users.noreply.github.com>
2026-04-13 10:18:56 +0000

    deploy: 8f6629632b0dfe899a3e839a5b09da20d1de0d53

--- dist/zn.d.ts
+++ dist/zn.d.ts
@@ -2281,7 +2281,7 @@ declare module "components/select/select.component" {
         helpText: string;
         /**
          * By default, form controls are associated with the nearest containing `<form>` element. This attribute allows you
-         * to place the form control outside of a form and associate it with the form that has this `id`. The form must be in
+         * to place the form control outside a form and associate it with the form that has this `id`. The form must be in
          * the same document or shadow root for this to work.
          */
         form: string;

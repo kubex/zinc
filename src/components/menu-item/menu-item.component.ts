553f75fe444d388bc709bcde118ef9a56435d48b
Alan Jones <alan.jones@chargehive.com>
2026-06-15 12:15:48 +0100

    update menu item check to use lucide icon

--- src/components/menu-item/menu-item.component.ts
+++ src/components/menu-item/menu-item.component.ts
@@ -239,7 +239,7 @@ export default class ZnMenuItem extends ZincElement {
         ?aria-expanded="${isSubmenuExpanded}">
         ${this.checkedPosition === 'left' ? html`
           <span part="checked-icon" class="menu-item__check">
-            <zn-icon src="check_small" aria-hidden="true"></zn-icon>
+            <zn-icon src="check@lu" size="18" aria-hidden="true"></zn-icon>
           </span>` : ''}
 
         <slot name="prefix" part="prefix" class="menu-item__prefix"></slot>
@@ -250,7 +250,7 @@ export default class ZnMenuItem extends ZincElement {
 
         ${this.checkedPosition === 'right' ? html`
           <span part="checked-icon" class="menu-item__check">
-            <zn-icon src="check_small" aria-hidden="true"></zn-icon>
+            <zn-icon src="check@lu" size="18" aria-hidden="true"></zn-icon>
           </span>` : ''}
 
         <span part="submenu-icon" class="menu-item__chevron">
